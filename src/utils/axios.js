acc507ec1a9e79f387a5e764c22c36409f7f5ba1
daniil <you@example.com>
2022-09-17 16:50:22 +0400

    16.09.22

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // export const baseURL = 'http://localhost:4444'
-export const baseURL = 'https://kryuchkov-mern.herokuapp.com/'
-// export const baseURL = process.env.REACT_APP_API_URL
+// export const baseURL = 'https://kryuchkov-mern.herokuapp.com/'
+export const baseURL = process.env.REACT_APP_API_URL
 
 const instance = axios.create({ baseURL })
 
